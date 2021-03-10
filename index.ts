a9c690e64d35dcbd5c275803de442b20c9f2bb92
BenReichwein <ben.basque@icloud.com>
2021-03-09 20:47:04 -0500

    added trust proxy to try and fix security issue

--- index.ts
+++ index.ts
@@ -19,6 +19,7 @@ const corsOptions = {
   credentials: true,
   exposedHeaders: ["token"]
 }
+app.enable('trust proxy');
 app.use(cors(corsOptions));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
--- routes/user.ts
+++ routes/user.ts
@@ -111,7 +111,7 @@ const user = (app: Router) => {
                 const token: string = jwt.sign(payload, secret, {
                     expiresIn: '7d'
                 });
-                res.cookie('token', token, { httpOnly: false, secure: true }).sendStatus(200);
+                res.cookie('token', token, { httpOnly: true, secure: true }).sendStatus(200);
                 }
             });
             }
@@ -187,7 +187,7 @@ const user = (app: Router) => {
     })
     // Logs out user
     app.get('/user/logout', function(req: Request, res: Response) {
-        res.cookie('token', "", { httpOnly: false }).sendStatus(200);
+        res.cookie('token', "", { httpOnly: true }).sendStatus(200);
     });
     // Check token with middleware
     app.get('/checkToken', withAuth, function(req: Request, res: Response) {
