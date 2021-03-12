a94347b7e5e8fd053c882fb92337babcc1b2b26b
BenReichwein <ben.basque@icloud.com>
2021-03-11 23:11:12 -0500

    Fixing compare passwords issue

--- models/User.ts
+++ models/User.ts
@@ -21,19 +21,23 @@ const UserSchema = new mongoose.Schema({
 });
 
 UserSchema.pre('save', function (this: any, next: any) {
-  if (this.isNew || this.isModified('password')) {
-    const document = this;
-    bcrypt.hash(this.password, saltRounds, function(err: any, hashedPassword: string) {
-      if (err) {
-        next(err);
-      } else {
-        document.password = hashedPassword;
-        next();
-      }
+    const user = this;
+
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified('password')) return next();
+
+    // generate a salt
+    bcrypt.genSalt(saltRounds, function(err: any, salt: any) {
+        if (err) return next(err);
+
+        // hash the password using our new salt
+        bcrypt.hash(user.password, salt, function(err: any, hash: any) {
+            if (err) return next(err);
+            // override the cleartext password with the hashed one
+            user.password = hash;
+            next();
+        });
     });
-  } else {
-    next();
-  }
 });
 
 UserSchema.methods.isCorrectPassword = function(password: string, callback: any) {
